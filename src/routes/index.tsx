a9819984452c0febcd63a00e41bf464ab67f33b6
Yongjie Xie 谢师傅 <xieyongjie2012@163.com>
2026-04-12 14:48:18 -0400

    feat: remove unused user authentication logic and delete button from IndexPage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,6 @@
 import { supabaseClient } from "../libs/supabaseClient"
-import { useAuthStore } from "../stores/useAuthStore";
 
 export const IndexPage = () => {
-  const user = useAuthStore(state => state.user);
-
 
   return (
     <>
@@ -11,18 +8,6 @@ export const IndexPage = () => {
         <p>Welcome to the landing page!</p>
         <div className=" flex flex-col gap-5">
           <button onClick={() => supabaseClient.auth.signOut()}>log out</button>
-          <button onClick={async () => {
-            console.log("当前登录的用户:", user?.id);
-
-            // 2. 执行删除操作
-            const res = await supabaseClient
-              .from("documents")
-              .delete()
-              .eq("id", 111);
-
-            console.log("删除结果:", res);
-          }}
-            className="w-48 mx-auto py-1 bg-amber-600">delete documents</button>
         </div>
       </div>
     </>
